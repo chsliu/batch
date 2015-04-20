0c9bfcb49d535e45fed350c8fa8c49f8333360af
Sita Liu <chsliu+github@gmail.com>
2015-04-20 22:16:36 +0800

    reorder Disk report

--- status.bat
+++ status.bat
@@ -184,15 +184,15 @@ wmic logicaldisk get DeviceID,FileSystem,FreeSpace,MaximumComponentLength,MediaT
 
 if defined POWERSHELL (
 echo ---------------------------------					>>%LOG1%
-Echo "Get-PhysicalDisk"							>>%LOG1%
-powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-Table -Auto}"	>>%LOG1%
-powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-List FriendlyName,OperationalStatus,HealthStatus,BusType,MediaType,Manufacturer,Model,Size,UniqueId}" 				>>%LOG1%
+Echo "Get-Volume"							>>%LOG1%
+powershell -command "if (Get-Command Get-Volume -errorAction SilentlyContinue) {Get-Volume | Format-Table -Auto}"		>>%LOG1%
 Echo "Get-VirtualDisk"							>>%LOG1%
 powershell -command "if (Get-Command Get-VirtualDisk -errorAction SilentlyContinue) {Get-VirtualDisk | Format-Table -Auto}"	>>%LOG1%
 Echo "Get-StoragePool"							>>%LOG1%
 powershell -command "if (Get-Command Get-StoragePool -errorAction SilentlyContinue) {Get-StoragePool | Format-Table -Auto}"	>>%LOG1%
-Echo "Get-Volume"							>>%LOG1%
-powershell -command "if (Get-Command Get-Volume -errorAction SilentlyContinue) {Get-Volume | Format-Table -Auto}"		>>%LOG1%
+Echo "Get-PhysicalDisk"							>>%LOG1%
+powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-Table -Auto}"	>>%LOG1%
+powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-List FriendlyName,OperationalStatus,HealthStatus,BusType,MediaType,Manufacturer,Model,Size,UniqueId}" 				>>%LOG1%
 )
 
 echo ---------------------------------					>>%LOG1%
