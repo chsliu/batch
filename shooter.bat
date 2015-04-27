cb51780b2b72ccee2620f602b37a37a9dd374354
Sita Liu <chsliu@gmail.com>
2015-04-27 17:51:51 +0800

    Automated commit at 2015/04/27 週一  下午 05:51   on VMW7

--- shooter.bat
+++ shooter.bat
@@ -7,6 +7,14 @@ REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media2\TV"
 
 REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media2\Movie"
 
-call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Library\TV Shows"
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Library"
 
-call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Library\Movies"
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Media"
+
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Library"
+
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media"
+
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Library"
+
+call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Media"
