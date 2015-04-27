9cb05f6f0084ddeec238db7bbfdc1dfd25244042
Sita Liu <chsliu@gmail.com>
2015-04-27 18:17:21 +0800

    Automated commit at 2015/04/27 週一  下午 06:17   on VMW7

---
+++ taskschd/daily-VMW7.bat
@@ -0,0 +1,11 @@
+call %~dp0\alarm.bat
+
+call %~dp0\gitsync.bat
+
+call %~dp0\shooter.bat
+
+rem call %~dp0\backup.bat
+
+rem %~dp0\..\networkutil\pinggoodview.bat
+
+rem %~dp0\..\utility\reboot.bat
---
+++ taskschd/weekly-VMW7.bat
@@ -0,0 +1,1 @@
+rem call %~dp0\status.bat
