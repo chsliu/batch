0c2f6158cc69202a036b7c3242faf7c32af83270
Sita Liu <chsliu@gmail.com>
2015-04-27 22:07:06 +0800

    Automated commit at Mon 04/27/2015  10:07 PM   on HV4

---
+++ utility/cleanup-HV4.bat
@@ -0,0 +1,19 @@
+REM =================================
+
+call %~dp0\cleanup.bat
+
+REM =================================
+
+rd /s /q E:\DEL	
+rd /s /q E:\Shares\Admin\DEL	
+rd /s /q E:\Shares\Library\DEL	
+rd /s /q E:\Shares\Media\DEL
+rd /s /q E:\Shares\NetBackup\DEL
+
+REM =================================
+
+set path=%path%;"E:\Shares\Admin\PortableApps\Portable Python 2.7.6.1\App"
+
+REM cleanup, keep last 3 dir, debug is 0(off)
+call E:\Shares\NetBackup\recycle\cleanup.py.cmd 3 0 
+
