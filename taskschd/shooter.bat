f9f2c9bf33687afdf2f2a8fd04da97d9ec465116
Sita Liu <chsliu+github@gmail.com>
2015-04-27 18:20:31 +0800

    Automated commit at 2015/04/27 週一  下午 06:20   on NB19

--- taskschd/shooter.bat
+++ taskschd/shooter.bat
@@ -1,12 +1,3 @@
-
-REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media\TV"
-
-REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media\Movie"
-
-REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media2\TV"
-
-REM call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media2\Movie"
-
 call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Library"
 
 call %~dp0\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Media"
---
+++ utility/timesync.bat
@@ -0,0 +1,1 @@
+w32tm /config /update /manualpeerlist:"time.stdtime.gov.tw clock.stdtime.gov.tw tick.stdtime.gov.tw tock.stdtime.gov.tw watch.stdtime.gov.tw,0x1"
