48b3f3578821e999db93e509cbe786b41d8c0f99
Sita Liu <chsliu@gmail.com>
2015-05-22 06:20:23 +0800

    Automated commit at 2015/05/22 週五  上午 06:20   on G2

--- taskschd/daily-G2.bat
+++ taskschd/daily-G2.bat
@@ -1,11 +1,11 @@
 REM =================================
 
-start /min call %~dp0\alarm.bat
+start /min /b call %~dp0\alarm.bat
 
 REM =================================
 
-start /min call %~dp0\gitsync.bat
+start /min /b call %~dp0\gitsync.bat
 
 REM =================================
 
-start /min rem call %~dp0\backup.bat
+start /min /b rem call %~dp0\backup.bat
