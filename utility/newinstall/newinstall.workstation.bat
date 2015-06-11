13d9c9c5c217d9ecc44e94ff752abc53cc43087d
Sita Liu <chsliu@gmail.com>
2015-06-11 17:27:47 +0800

    Automated commit at 2015/06/11 週四  下午 05:27   on G2

--- utility/newinstall/newinstall.workstation.bat
+++ utility/newinstall/newinstall.workstation.bat
@@ -50,30 +50,18 @@ REM openhardwaremonitor
 REM opera
 REM picasa
 REM pidgin
-<<<<<<< HEAD
 REM pushbullet
-=======
-REM pushbullet 
->>>>>>> 0ba72cb27c91511596d1ff837acb674479276af0
 REM spacesniffer
 REM stickies
 REM sumatrapdf
 REM teamviewer.host
-<<<<<<< HEAD
 REM upx
-=======
->>>>>>> 0ba72cb27c91511596d1ff837acb674479276af0
 REM wincdemu
 REM winsplitrevolution
 REM xnview
-<<<<<<< HEAD
 REM zim
 
-cinst -y 7zip.install classic-shell cmdermini.portable dropbox everything foxitreader google-chrome-x64 line listary notepadplusplus.install openhardwaremonitor python2-x86_32 skype teamviewer git.install
-=======
-
-cinst -y 7zip.install classic-shell cmdermini.portable dropbox everything foxitreader google-chrome-x64 line listary notepadplusplus.install  python2 skype teamviewer git.install
+cinst -y 7zip.install classic-shell cmdermini.portable dropbox everything foxitreader google-chrome-x64 line listary notepadplusplus.install python2-x86_32 skype teamviewer git.install
 
 pause
->>>>>>> 0ba72cb27c91511596d1ff837acb674479276af0
 
