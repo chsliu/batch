ad7bbac1bddb4b963ef6a11e816ffe9f8d205075
Sita Liu <chsliu@gmail.com>
2015-06-11 21:20:00 +0800

    Automated commit at 2015/06/11 週四  下午 09:19   on MACMINI

---
+++ utility/newinstall/macmini.bat
@@ -0,0 +1,36 @@
+@echo off
+
+REM =================================
+:BatchGotAdmin
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    if '%1'=='UACdone' (shift & goto gotAdmin)
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~0", "UACdone", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM =================================
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+REM =================================
+
+cinst -y 7zip git kodi microsoftsecurityessentials teamviewer GoogleChrome python2
+
+pause
+
