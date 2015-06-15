7dc1caf03c27cda17aa8d76bc7c5a3131bdd0761
Sita Liu <chsliu@gmail.com>
2015-06-16 06:28:36 +0800

    Automated commit at Tue 06/16/2015  06:28 AM   on HV3

--- rsync/daily-HV3.bat
+++ rsync/daily-HV3.bat
@@ -4,7 +4,7 @@ call %~dp0\backup E:\Shares\Admin
 call %~dp0\backup E:\Shares\Photos
 rem call %~dp0\backup E:\Shares\Video
 call %~dp0\backup E:\Shares\Users
-call %~dp0\backup E:\Shares\Music
+rem call %~dp0\backup E:\Shares\Music
 call %~dp0\backup E:\Shares\Software
 rem call %~dp0\backup E:\Shares\NetBackup
 
