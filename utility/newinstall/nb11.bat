aeda773fb045f9a75cf3b609ec801110e5e17194
Sita Liu <chsliu@gmail.com>
2015-06-18 17:46:41 +0800

    Automated commit at ?? 2015/06/18  ?? 05:45   on NB11

---
+++ utility/newinstall/nb11.bat
@@ -0,0 +1,36 @@
+@echo off
+
+REM =================================
+:BatchGotAdmin
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    if '%1'=='UACdone' (shift & goto gotAdmin)
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~0", "UACdone", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM =================================
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+REM =================================
+
+cinst -y 7zip everything git GoogleChrome python2 microsoftsecurityessentials teamviewer cmder f.lux potplayer foxitreader notepadplusplus putty teracopy
+
+pause
+
