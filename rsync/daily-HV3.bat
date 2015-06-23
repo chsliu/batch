f7327ded61b5308ec577db5cbd67b6f51c60e1ea
Sita Liu <chsliu@gmail.com>
2015-06-23 20:16:36 +0800

    Automated commit at 2015/06/23 週二  下午 08:16   on G2

--- rsync/daily-HV3.bat
+++ rsync/daily-HV3.bat
@@ -1,11 +1,11 @@
 @echo off
 
 call %~dp0\backup E:\Shares\Admin\hv3
-call %~dp0\backup E:\Shares\Photos
-call %~dp0\backup E:\Shares\Software
-call %~dp0\backup E:\Shares\Users
 rem call %~dp0\backup E:\Shares\Music
 rem call %~dp0\backup E:\Shares\NetBackup
+rem call %~dp0\backup E:\Shares\Photos
+rem call %~dp0\backup E:\Shares\Software
+rem call %~dp0\backup E:\Shares\Users
 rem call %~dp0\backup E:\Shares\Video
 
 goto exit
