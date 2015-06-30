05a8459fe74052918f5b81c0263d61f2f38a292b
Sita Liu <chsliu@gmail.com>
2015-06-30 17:00:06 +0800

    Automated commit at 2015/06/30 週二  下午 05:00   on NB19

--- taskschd/daily-HV4.bat
+++ taskschd/daily-HV4.bat
@@ -2,14 +2,7 @@ REM =================================
 
 call e:\Shares\Media\userdata\gitsync.bat
 
-<<<<<<< HEAD
 REM =================================
 
 call E:\Shares\Software\UNIX\Lubuntu\xbmc\userdata\gitsync.bat
 
-REM =================================
-
-call %~dp0\backup.bat
-
-=======
->>>>>>> 452ababd699617d1ea46960f7bdb826adb200e99
