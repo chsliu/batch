62349a3ae09b11d1a911fabc72cdeb5b2540d7e9
Sita Liu <chsliu@gmail.com>
2015-07-17 15:15:14 +0800

    Automated commit at 2015/07/17 週五  下午 03:15   on NB19

--- startup.bat
+++ startup.bat
@@ -8,6 +8,10 @@ call %~dp0\ShooterSubPyDownloader\gitsync.bat
 
 REM =================================
 
+call %~dp0\utility\alarm.bat
+
+REM =================================
+
 call %~dp0\taskschd\%~n0.bat
 
 REM =================================
