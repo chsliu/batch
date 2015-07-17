f227e524f0fca97fd0d5c400acb71a97fa8f99e5
Sita Liu <chsliu@gmail.com>
2015-07-18 06:25:17 +0800

    Automated commit at Sat 07/18/2015  06:25 AM   on HV3

--- utility/getphydisk.bat
+++ utility/getphydisk.bat
@@ -1,3 +1,15 @@
-powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-Table -Auto}" 
+@echo off
+
+Echo "Get-Volume"							
+powershell -command "if (Get-Command Get-Volume -errorAction SilentlyContinue) {Get-Volume | Format-Table -Auto}"		
+Echo "Get-VirtualDisk"							
+powershell -command "if (Get-Command Get-VirtualDisk -errorAction SilentlyContinue) {Get-VirtualDisk | Format-Table -Auto}"	
+
+Echo "Get-StoragePool"							
+powershell -command "if (Get-Command Get-StoragePool -errorAction SilentlyContinue) {Get-StoragePool | Format-Table -Auto}"	
+
+Echo "Get-PhysicalDisk"							
+powershell -command "if (Get-Command Get-PhysicalDisk -errorAction SilentlyContinue) {Get-PhysicalDisk | Format-Table -Auto}"	
+
 
 rem E:\Shares\Admin\PortableApps\OpenHardwareMonitor\OpenHardwareMonitor.exe
