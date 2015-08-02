08753e90a6c0aef1557c9fbcd481219e8b143f9e
Sita Liu <chsliu@gmail.com>
2015-08-02 12:28:02 +0800

    Automated commit at 2015/08/02 週日  下午 12:27   on W1

--- utility/newinstall/nb11.bat
+++ utility/newinstall/nb11.bat
@@ -15,6 +15,9 @@ REM microsoftsecurityessentials
 
 cinst -y 7zip cmder Everything f.lux git GoogleChrome notepadplusplus potplayer putty python2 teamviewer 
 
+cd/d d:\Users\sita\Documents
+git clone https://github.com/chsliu/batch.git
+
 rem pause
 
 C:\Windows\System32\timeout.exe 10
--- utility/newinstall/w1.bat
+++ utility/newinstall/w1.bat
@@ -11,6 +11,25 @@ REM =================================
 
 cinst -y 7zip classic-shell cmder cpu-z crystaldiskinfo crystaldiskmark dropbox everything f.lux filebot firefox foxitreader git github google-chrome-x64 gow hg kodi line listary mp3tag netscan64 nimbletext notepadplusplus paint.net potplayer putty python2 skype teamviewer teraterm visualstudioexpress2008 windowsliveinstaller winmerge winscp wireshark
 
+cd/d d:\Users\sita\Documents
+git clone https://github.com/chsliu/batch.git
+
+REM startup
+REM d:\Users\sita\Documents\tasks\startup.bat
+REM daily
+REM d:\Users\sita\Documents\tasks\daily.bat
+REM weekly
+REM d:\Users\sita\Documents\tasks\weekly.bat
+REM monthly
+REM d:\Users\sita\Documents\tasks\monthly.bat
+
+cd/d batch
+git clone https://github.com/chsliu/ShooterSubPyDownloader.git
+
+mkdir %appdata%\kodi
+cd/d %appdata%\kodi
+git clone https://github.com/chsliu/userdata.git
+
 rem pause
 
 C:\Windows\System32\timeout.exe 10
