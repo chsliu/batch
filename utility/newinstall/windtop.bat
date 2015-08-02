ef56d265b3a975afda55f9a0b7c959329b58d934
Sita Liu <chsliu@gmail.com>
2015-08-02 15:00:35 +0800

    Automated commit at 2015/08/02 週日  下午 03:00   on DESKTOP-U8T1Q75

---
+++ utility/newinstall/windtop.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM =================================
+if [%1]==[] %~dp0\..\getadmin.bat "%~dp0\%~nx0"
+
+REM =================================
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+REM =================================
+
+REM classic-shell
+REM foxitreader
+REM microsoftsecurityessentials
+
+cinst -y 7zip cmder Everything f.lux git GoogleChrome notepadplusplus potplayer putty python2 teamviewer kodi
+
+cd/d d:\Users\sita\Documents
+git clone https://github.com/chsliu/batch.git
+
+rem pause
+
+C:\Windows\System32\timeout.exe 10
