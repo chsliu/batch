dbf5fd4688b53faeea13573cfbc9d73fd8b22be4
Sita Liu <chsliu@gmail.com>
2015-08-07 16:30:03 +0800

    Automated commit at 2015/08/07 週五  下午 04:30   on W1

--- utility/newinstall/w1.bat
+++ utility/newinstall/w1.bat
@@ -11,6 +11,8 @@ REM =================================
 
 cinst -y 7zip classic-shell cmder cpu-z crystaldiskinfo crystaldiskmark dropbox everything f.lux filebot firefox foxitreader git github google-chrome-x64 gow hg kodi line listary mp3tag netscan64 nimbletext notepadplusplus paint.net potplayer putty python2 skype teamviewer teraterm visualstudioexpress2008 windowsliveinstaller winmerge winscp wireshark
 
+choco feature enable -nautoUninstaller
+
 cd/d d:\Users\sita\Documents
 git clone https://github.com/chsliu/batch.git
 
