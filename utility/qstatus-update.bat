6f42ae67078226f1dd60bfbcae23665f03fccd02
Sita Liu <chsliu@gmail.com>
2015-08-11 19:15:35 +0800

    Automated commit at 2015/08/11 週二  下午 07:15   on W1

---
+++ utility/qstatus-update.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set path=%path%;%~dp0\..\bin
+set TEMPPATH=%temp%\qstatus
+set DSTPATH=D:\Users\sita\Dropbox\Public
+
+mkdir %TEMPPATH%
+mkdir %TEMPPATH%\bin
+mkdir %TEMPPATH%\utility
+copy %~dp0\status.bat %TEMPPATH%\utility
+copy %~dp0\getadmin.bat %TEMPPATH%\utility
+copy %~dp0\..\bin\sendEmail.exe %TEMPPATH%\bin
+copy %~dp0\..\bin\smartctl.exe %TEMPPATH%\bin
+
+7za a -mx9 %~dp0\qstatus.7z %TEMPPATH%
+move %~dp0\qstatus.7z %DSTPATH%
+
+rd /s /q %TEMPPATH%
--- utility/status.bat
+++ utility/status.bat
@@ -37,8 +37,30 @@ set LOG8=%temp%\coreinfo.txt
 set TXT1=%temp%\%~n0.txt
 
 REM =================================
+goto :Main
+
+REM =================================
+:whereis
+set %2=
+for %%X in (%1) do (set %2=%%~$PATH:X)
+
+exit /b
+
+REM =================================
+:findtext
+>nul find %2 %1 && (
+  echo %2 was found.
+  set %3=1
+) || (
+  echo %2 was NOT found.
+)
+
+exit /b
+
+REM =================================
 REM Gathering System Report
 REM =================================
+:Main
 
 if not exist %LOG2% systeminfo >%LOG2% 2>>&1
 echo. >>%LOG2%
@@ -249,22 +271,3 @@ sendemail -s msa.hinet.net -f egreta.su@msa.hinet.net -t chsliu@gmail.com -u [LO
 rem %LOG2% %LOG3% %LOG3CAB% %LOG4% %LOG6% %LOG6NFO% %LOG6CAB%
 del %LOG1% %LOG5% %LOG7% %TXT1%
 
-goto :EOF
-
-REM =================================
-:whereis
-set %2=
-for %%X in (%1) do (set %2=%%~$PATH:X)
-
-exit /b
-
-REM =================================
-:findtext
->nul find %2 %1 && (
-  echo %2 was found.
-  set %3=1
-) || (
-  echo %2 was NOT found.
-)
-
-exit /b
