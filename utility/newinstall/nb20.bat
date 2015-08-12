3e30941236917960fbd4dff2087ae5b6b43f384f
Sita Liu <chsliu@gmail.com>
2015-08-12 16:18:30 +0800

    Automated commit at 2015/08/12 週三  下午 04:18   on DESKTOP-KFPLB5J

---
+++ utility/newinstall/nb20.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM =================================
+if [%1]==[] %~dp0\..\getadmin.bat "%~dp0\%~nx0"
+
+REM =================================
+set path=%path%;C:\Program Files (x86)\Git\cmd
+
+REM =================================
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+REM =================================
+
+choco feature enable -nautoUninstaller
+
+cinst -y 7zip cmder f.lux git kodi potplayer putty teamviewer python2
+
+mkdir d:\Users\sita\Documents
+cd/d d:\Users\sita\Documents
+git clone https://github.com/chsliu/batch.git
+
+mkdir %appdata%\Kodi
+cd/d %appdata%\Kodi
+git clone https://github.com/chsliu/userdata.git
+
+rem pause
+
+C:\Windows\System32\timeout.exe 10
