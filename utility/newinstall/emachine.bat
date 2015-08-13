4974925f63dfd45934984c3bcd8a9faaae7aef72
Sita Liu <chsliu@gmail.com>
2015-08-13 09:06:37 +0800

    Automated commit at 2015/08/13 週四  上午 09:06   on W1

--- utility/newinstall/emachine.bat
+++ utility/newinstall/emachine.bat
@@ -7,6 +7,10 @@ REM =================================
 set path=%path%;C:\Program Files (x86)\Git\cmd
 set DOC=d:\Users\%USERNAME%\Documents
 set TASKS=%DOC%\tasks
+set SCHEDULE=16:30-17:00-17:30
+set DAILY=%SCHEDULE:~0,5%
+set WEEKLY=%SCHEDULE:~6,5%
+set MONTHLY=%SCHEDULE:~12,5%
 set PASS=1
 
 REM =================================
@@ -31,9 +35,9 @@ cd/d %DOC%
 git clone https://github.com/chsliu/batch.git tasks
 
 SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC ONSTART /TN "%USERNAME%-startup" /TR "%TASKS%\startup.bat"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "%TASKS%\daily.bat" /ST 16:30
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "%TASKS%\weekly.bat" /ST 17:00
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "%TASKS%\monthly.bat" /ST 17:30
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "%TASKS%\daily.bat" /ST %DAILY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "%TASKS%\weekly.bat" /ST %WEEKLY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "%TASKS%\monthly.bat" /ST %MONTHLY%
 
 REM =================================
 
--- utility/newinstall/taskset.bat
+++ utility/newinstall/taskset.bat
@@ -1,22 +1,19 @@
 REM =================================
 if [%1]==[] %~dp0\..\..\utility\getadmin.bat "%~dp0\%~nx0"
 
+REM =================================
+set DOC=d:\Users\%USERNAME%\Documents
+set TASKS=%DOC%\tasks
+set SCHEDULE=16:30-17:00-17:30
+set DAILY=%SCHEDULE:~0,5%
+set WEEKLY=%SCHEDULE:~6,5%
+set MONTHLY=%SCHEDULE:~12,5%
 set PASS=1
 
 REM =================================
-SchTasks /Delete /F /TN "startup"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC ONSTART /TN "%USERNAME%-startup" /TR "d:\Users\%USERNAME%\Documents\tasks\startup.bat"
-
-REM =================================
-SchTasks /Delete /F /TN "daily"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "d:\Users\%USERNAME%\Documents\tasks\daily.bat" /ST 16:30
-
-REM =================================
-SchTasks /Delete /F /TN "weekly"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "d:\Users\%USERNAME%\Documents\tasks\weekly.bat" /ST 17:00
-
-REM =================================
-SchTasks /Delete /F /TN "monthly"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "d:\Users\%USERNAME%\Documents\tasks\monthly.bat" /ST 17:30
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC ONSTART /TN "%USERNAME%-startup" /TR "%TASKS%\startup.bat"
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "%TASKS%\daily.bat" /ST %DAILY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "%TASKS%\weekly.bat" /ST %WEEKLY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "%TASKS%\monthly.bat" /ST %MONTHLY%
 
 pause
