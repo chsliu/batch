963ef023b35959507a34a20f6dcb4376f3a86655
Sita Liu <chsliu@gmail.com>
2015-08-17 14:42:42 +0800

    Automated commit at 2015/08/17 週一  下午 02:41   on G2

--- utility/newinstall/g2.bat
+++ utility/newinstall/g2.bat
@@ -5,9 +5,9 @@ if [%1]==[] %~dp0\..\getadmin.bat "%~dp0\%~nx0"
 
 REM =================================
 set path=%path%;C:\Program Files (x86)\Git\cmd
-set DOC=d:\Users\%USERNAME%\Documents
+set DOC=c:\Users\%USERNAME%\Documents
 set TASKS=%DOC%\tasks
-set SCHEDULE=16:30-17:00-17:30
+set SCHEDULE=06:05-06:35-07:05
 set DAILY=%SCHEDULE:~0,5%
 set WEEKLY=%SCHEDULE:~6,5%
 set MONTHLY=%SCHEDULE:~12,5%
@@ -21,7 +21,7 @@ REM =================================
 
 choco feature enable -nautoUninstaller
 
-cinst -y 7zip classic-shell cmder cpu-z crystaldiskinfo crystaldiskmark dropbox everything f.lux filebot firefox foxitreader git github google-chrome-x64 gow hg kodi line listary mp3tag netscan64 nimbletext notepadplusplus paint.net potplayer putty python2 skype teamviewer teraterm visualstudioexpress2008 windowsliveinstaller winmerge winscp wireshark
+cinst -y 7zip cmder cpu-z crystaldiskinfo crystaldiskmark dropbox everything f.lux filebot firefox git github google-chrome-x64 kodi notepadplusplus potplayer putty python2 skype teamviewer winscp wireshark
 
 REM =================================
 
@@ -36,7 +36,7 @@ SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /
 
 REM =================================
 
-cd/d batch
+cd/d tasks
 git clone https://github.com/chsliu/ShooterSubPyDownloader.git
 
 REM =================================
