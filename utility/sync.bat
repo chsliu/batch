cbf9072a71d2c67c417f055dc394e41beb6f740b
Sita Liu <chsliu@gmail.com>
2015-09-03 18:39:05 +0800

    Automated commit at 2015/09/03 週四  下午 06:39   on W1

--- utility/sync.bat
+++ utility/sync.bat
@@ -46,7 +46,7 @@ REM =================================
 copy %0 %TXT1% >nul
 
 if defined ALARM (
-sendemail -s msa.hinet.net -f egreta.su@msa.hinet.net -t chsliu@gmail.com -u [LOG] %COMPUTERNAME% %~n0 -m %0 -a %LOG1% %TXT1%
+sendemail -s msa.hinet.net -f egreta.su@msa.hinet.net -t chsliu@gmail.com -u [LOG] %COMPUTERNAME% %~n0 ERROR -m %0 -a %LOG1% %TXT1%
 )
 
 del %LOG1% %TXT1% %LINE%
