e122a013e9cb7449cb7610188a920a7062ac73d7
Sita Liu <chsliu@gmail.com>
2015-10-21 16:30:07 +0800

    Automated commit at 2015/10/21 週三  下午 04:30   on W1

--- wiiiso2wbfs/iso2wbfs.py
+++
@@ -1,8 +0,0 @@
-import os
-for file in os.listdir('.'):
-    if file.endswith('.iso'):
-        os.system('wbfs_file.exe "%s"' % file)
-        os.system('del "%s"' % file)
-
-os.system('del *.url')
-os.system('del *.txt')
