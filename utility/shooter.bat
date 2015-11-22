04b9b6a3971cb12499bb3180c8d49a532c71dc70
Sita Liu <chsliu@gmail.com>
2015-11-23 06:24:52 +0800

    Automated commit at 2015/11/23 週一  上午 06:24   on VMW7

--- utility/shooter.bat
+++ utility/shooter.bat
@@ -1,11 +1,11 @@
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Library"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv2\Library"
 
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv2\Media"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv2\Media"
 
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Library"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv3\Library"
 
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv3\Media"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv3\Media"
 
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Library"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv4\Library"
 
-call %~dp0\..\ShooterSubPyDownloader\shootersuball.bat "\\hv4\Media"
+call %~dp0\..\ShooterSubPyDownloader\sendemail.shootersuball.bat "\\hv4\Media"
