045bebea0d96b8bbdcc6ce37e16a82ec581d1bc2
Sita Liu <chsliu@gmail.com>
2016-01-10 17:52:56 +0800

    Automated commit at 2016/01/10 週日  下午 05:52   on NB19

---
+++ util/m3u.timestamp.cmd
@@ -0,0 +1,90 @@
+@setlocal enableextensions & python -x "%~f0" %* & goto :EOF
+
+
+from __future__ import print_function
+import sys
+from datefilter import datefilter
+
+
+def usage(prog):
+    # print "%s <inputfile> <pattern> [|Reverse|Nosort]" % prog
+    print("%s <pattern> [Reverse]" % prog) 
+
+
+def win32_unicode_argv():
+    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
+    strings.
+
+    Versions 2.x of Python don't support Unicode in sys.argv on
+    Windows, with the underlying Windows API instead replacing multi-byte
+    characters with '?'.
+    """
+
+    from ctypes import POINTER, byref, cdll, c_int, windll
+    from ctypes.wintypes import LPCWSTR, LPWSTR
+
+    GetCommandLineW = cdll.kernel32.GetCommandLineW
+    GetCommandLineW.argtypes = []
+    GetCommandLineW.restype = LPCWSTR
+
+    CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+    CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+    CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+    cmd = GetCommandLineW()
+    argc = c_int(0)
+    argv = CommandLineToArgvW(cmd, byref(argc))
+    if argc.value > 0:
+        # Remove Python executable and commands if present
+        start = argc.value - len(sys.argv)
+        return [argv[i] for i in
+                xrange(start, argc.value)]
+
+
+def warning_item(*objs):
+	for obj in objs:
+		if isinstance(obj,unicode): obj=unicode(obj).encode(sys.stderr.encoding,'replace')
+		print(obj,file=sys.stderr, end=" ")
+
+
+def warning(*objs):
+	for obj in objs: warning_item(obj)
+	print("",file=sys.stderr)
+
+
+def today():
+	from datetime import date
+	return date.today().strftime("%Y%m%d")
+	
+	
+def timestamp(f):
+	# table = {}
+	line = f.readline()
+	while line:
+		line = line.strip()
+		if len(line) and line[0] == '#':
+			url = f.readline().strip()
+			if url: 
+				# table[line]=url
+				dates,rule = datefilter(line.decode('utf-8'))
+				if not dates: 	print(line,"["+today()+"]")
+				else: 			print(line)
+				print(url)	
+		else:
+			print("")
+		line = f.readline()
+	
+
+def main():
+	sys.argv = win32_unicode_argv()
+	if len(sys.argv) < 1:
+		usage(sys.argv[0])
+		return
+	
+	f = sys.stdin
+	
+	timestamp(f)
+	
+	
+if __name__ == '__main__':
+	main()
