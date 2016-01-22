5807e60fec642f702735b9805605c99989232a83
Sita Liu <chsliu@gmail.com>
2016-01-22 16:30:02 +0800

    Automated commit at 2016/01/22 週五  下午 04:30   on W1

--- taskschd/daily-W1.bat
+++ taskschd/daily-W1.bat
@@ -1,11 +1,11 @@
 REM =================================
 
-call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\YouTube\update.bat"
-call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\YouTube\run.daily.bat"
+REM call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\YouTube\update.bat"
+REM call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\YouTube\run.daily.bat"
 
 
-call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\update.bat"
-call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\run.daily.bat"
+REM call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\update.bat"
+REM call "C:\Users\sita\AppData\Roaming\Kodi\userdata\playlists\video\run.daily.bat"
 
 
 REM pushd \\hv4\Media\userdata\
