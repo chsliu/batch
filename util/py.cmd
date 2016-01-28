1da52db362c0452cc7defa1f39933f537c1c4826
Sita Liu <chsliu@gmail.com>
2016-01-28 16:30:12 +0800

    Automated commit at 2016/01/28 週四  下午 04:30   on NB19

--- util/py.cmd
+++ util/py.cmd
@@ -7,6 +7,39 @@ import inspect
 from optparse import OptionParser
 
 
+USAGE = "usage: %prog [options] arg1 arg2"
+
+
+def win32_unicode_argv():
+    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
+    strings.
+
+    Versions 2.x of Python don't support Unicode in sys.argv on
+    Windows, with the underlying Windows API instead replacing multi-byte
+    characters with '?'.
+    """
+
+    from ctypes import POINTER, byref, cdll, c_int, windll
+    from ctypes.wintypes import LPCWSTR, LPWSTR
+
+    GetCommandLineW = cdll.kernel32.GetCommandLineW
+    GetCommandLineW.argtypes = []
+    GetCommandLineW.restype = LPCWSTR
+
+    CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+    CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+    CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+    cmd = GetCommandLineW()
+    argc = c_int(0)
+    argv = CommandLineToArgvW(cmd, byref(argc))
+    if argc.value > 0:
+        # Remove Python executable and commands if present
+        start = argc.value - len(sys.argv)
+        return [argv[i] for i in
+                xrange(start, argc.value)]
+
+
 def warning_item(*objs):
 	for obj in objs:
 		if isinstance(obj,unicode): obj=unicode(obj).encode(sys.stderr.encoding,'replace')
@@ -26,8 +59,6 @@ def lineno():
 def parse(file):
 	pass
 
-
-USAGE = "usage: %prog [options] arg1 arg2"
 
 def main():
 	parser = OptionParser(USAGE)
@@ -59,7 +90,8 @@ if __name__ == '__main__':
 	import traceback
 	import time
 	
-	try: 	
+	try:
+		sys.argv = win32_unicode_argv()
 		main()
 	except:
 		traceback.print_exc()
