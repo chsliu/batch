7783ceddf52c426e93c09f11631626b453101a56
Sita Liu <chsliu@gmail.com>
2016-01-29 09:42:34 +0800

    Automated commit at 2016/01/29 週五  上午 09:42   on W1

--- utility/ramdisk_t_create_512m.bat
+++ utility/ramdisk_t_create_512m.bat
@@ -4,4 +4,4 @@ REM =================================
 if [%1]==[] %~dp0\..\utility\getadmin.bat "%~dp0\%~nx0"
 
 REM =================================
-imdisk -D -m R:
+imdisk -a -s 512M -m T: -p "/fs:ntfs /q /y"
--- utility/ramdisk_t_del.bat
+++ utility/ramdisk_t_del.bat
@@ -4,4 +4,4 @@ REM =================================
 if [%1]==[] %~dp0\..\utility\getadmin.bat "%~dp0\%~nx0"
 
 REM =================================
-imdisk -a -s 512M -m R: -p "/fs:ntfs /q /y"
+imdisk -D -m T:
