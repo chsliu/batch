353dd470c1d27f17a7954964755413bef94377d2
Sita Liu <chsliu@gmail.com>
2016-07-20 06:40:11 +0800

    Automated commit at 2016/07/20 週三  上午 06:40   on VMW7

--- utility/sendemail.filebot.amc.bat
+++ utility/sendemail.filebot.amc.bat
@@ -96,11 +96,7 @@ REM =================================
 set nofiletag=call "d:\Users\sita\Documents\tasks\util\show.no.filetag.cmd"
 
 for /f %%i in ('%nofiletag% %8 [eztv]') do set fname=%%i
-<<<<<<< HEAD
 if not [%fname%]==[] move "%8" "%fname%" >nul
-=======
-if not [%fname%]==[] move %8 "%fname%" >nul
->>>>>>> 1ce86de3dd733f139c074981dd2ce27bf426d1ab
 
 for /f %%i in ('%nofiletag% %5 [eztv]') do set fname=%%i
 if [%fname%]==[] set fname=%5
