2bf0aa4253f53cdf4dce277394d48e89e60ddbe3
Sita Liu <chsliu@gmail.com>
2016-09-09 16:30:02 +0800

    Automated commit at 2016/09/09 週五  下午 04:30   on W1

--- util/m3u.db.cmd
+++ util/m3u.db.cmd
@@ -13,6 +13,17 @@ from datetime import datetime, timedelta
 import chardet
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+	
+
 def usage(prog):
     # print "%s <inputfile> <pattern> [|Reverse|Nosort]" % prog
     print("%s <pattern> [Reverse]" % prog) 
@@ -96,7 +107,7 @@ def m3udb(file, db, onlynew):
 				print(url) 
 		else:
 			titleUnicode = title.decode('utf-8')
-			warning("[Title]",titleUnicode)
+			warning(bcolors.WARNING,"[Title]",titleUnicode,bcolors.ENDC)
 			db[url] = titleUnicode
 
 			print(title) 
--- util/py.cmd
+++ util/py.cmd
@@ -10,6 +10,17 @@ from optparse import OptionParser
 USAGE = "usage: %prog [options] arg1 arg2"
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+	
+
 def win32_unicode_argv():
     """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
     strings.
