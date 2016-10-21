509887e3928107287597a44852e32ff450f4dd31
Sita Liu <chsliu@gmail.com>
2016-10-21 16:30:03 +0800

    Automated commit at 2016/10/21 週五  下午 04:30   on NB19

---
+++ util/showmyip.cmd
@@ -0,0 +1,125 @@
+@setlocal enableextensions & python -x "%~f0" %* & goto :EOF
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+import inspect
+from optparse import OptionParser
+
+
+USAGE = "usage: %prog [options] arg1 arg2"
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+	
+
+def win32_unicode_argv():
+    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
+    strings.
+
+    Versions 2.x of Python don't support Unicode in sys.argv on
+    Windows, with the underlying Windows API instead replacing multi-byte
+    characters with '?'.
+    """
+
+    from ctypes import POINTER, byref, cdll, c_int, windll
+    from ctypes.wintypes import LPCWSTR, LPWSTR
+
+    GetCommandLineW = cdll.kernel32.GetCommandLineW
+    GetCommandLineW.argtypes = []
+    GetCommandLineW.restype = LPCWSTR
+
+    CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+    CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+    CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+    cmd = GetCommandLineW()
+    argc = c_int(0)
+    argv = CommandLineToArgvW(cmd, byref(argc))
+    if argc.value > 0:
+        # Remove Python executable and commands if present
+        start = argc.value - len(sys.argv)
+        return [argv[i] for i in
+                xrange(start, argc.value)]
+
+
+def warning_item(*objs):
+	for obj in objs:
+		if isinstance(obj,unicode): obj=unicode(obj).encode(sys.stderr.encoding,'replace')
+		print(obj,file=sys.stderr, end=" ")
+
+
+def warning(*objs):
+	for obj in objs: warning_item(obj)
+	print("",file=sys.stderr)
+
+
+def lineno():
+	"""Returns the current line number in our program."""
+	return inspect.currentframe().f_back.f_lineno
+
+	
+def parse(file):
+	from urllib2 import urlopen
+	my_ip = urlopen('http://ip.42.pl/raw').read()
+	print(my_ip)
+	return
+	
+	from json import load
+	from urllib2 import urlopen
+	my_ip = load(urlopen('http://jsonip.com'))['ip']
+	
+	from json import load
+	from urllib2 import urlopen
+	my_ip = load(urlopen('http://httpbin.org/ip'))['origin']
+
+	from json import load
+	from urllib2 import urlopen
+	my_ip = load(urlopen('https://api.ipify.org/?format=json'))['ip']
+
+
+def main():
+	parser = OptionParser(USAGE)
+	parser.add_option("-v", "--verbose",
+                  action="store_true", dest="verbose", default=True,
+                  help="make lots of noise [default]")
+	parser.add_option("-f", "--filename", metavar="FILE", help="write output to FILE")
+	opt, args = parser.parse_args()
+	
+	# if len(args) < 1:
+		# print(USAGE)
+		# sys.exit(1)
+		
+	# file_name = args[0]
+
+	# if opt.dummy: dummy = int(opt.dummy)
+	# else:         dummy = 0
+	
+	# try: pass
+	# except:
+		# warning(sys.exc_info()[0])
+		# warning(sys.exc_info())
+		
+	f = sys.stdin
+	parse(f)
+
+
+if __name__ == '__main__':
+	import traceback
+	import time
+	
+	try:
+		sys.argv = win32_unicode_argv()
+		main()
+	except:
+		traceback.print_exc()
+		warning("Press Ctrl-C to Continue...")
+		while True:  time.sleep(1)
