ab954bb8bcf39daa57506dcba95a3c06521d7e68
Sita Liu <chsliu@gmail.com>
2016-10-26 08:19:10 +0800

    Automated commit at 週三 2016/10/26  上午 08:19   on W1

--- batch/newinstall/newinstall.taskschd.bat
+++ batch/newinstall/newinstall.taskschd.bat
@@ -4,9 +4,11 @@ mkdir %DOC%
 pushd %DOC%
 git clone https://github.com/chsliu/batch.git tasks
 
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC ONSTART /TN "%USERNAME%-startup" /TR "%TASKS%\startup.bat"
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "%TASKS%\daily.bat" /ST %DAILY%
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "%TASKS%\weekly.bat" /ST %WEEKLY%
-SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "%TASKS%\monthly.bat" /ST %MONTHLY%
+setx TASKS_ROOT %DOC%\tasks
+
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC ONSTART /TN "%USERNAME%-startup" /TR "%TASKS_ROOT%\startup.bat"
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC DAILY /TN "%USERNAME%-daily" /TR "%TASKS_ROOT%\daily.bat" /ST %DAILY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC WEEKLY /D FRI /TN "%USERNAME%-weekly" /TR "%TASKS_ROOT%\weekly.bat" /ST %WEEKLY%
+SchTasks /Create /F /RL HIGHEST /RU %USERNAME% /RP %PASS% /SC MONTHLY /MO LAST /D FRI /TN "%USERNAME%-monthly" /TR "%TASKS_ROOT%\monthly.bat" /ST %MONTHLY%
 
 popd
--- batch/newinstall/tiroc.bat
+++ batch/newinstall/tiroc.bat
@@ -6,7 +6,7 @@ if [%1]==[] %~dp0\..\utility\getadmin.bat "%~dp0\%~nx0"
 REM =================================
 set path=%path%;C:\Program Files (x86)\Git\cmd
 set DOC=c:\Users\%USERNAME%\Documents
-set TASKS=%DOC%\tasks
+REM set TASKS=%DOC%\tasks
 set SCHEDULE=13:30-14:00-14:30
 set DAILY=%SCHEDULE:~0,5%
 set WEEKLY=%SCHEDULE:~6,5%
