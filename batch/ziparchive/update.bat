72dd23a08e7a328e160468c2f77f7fe50db082ad
Sita Liu <chsliu@gmail.com>
2016-10-26 08:14:55 +0800

    Automated commit at 週三 2016/10/26  上午 08:14   on W1

--- batch/ziparchive/update.bat
+++ batch/ziparchive/update.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set SRCDIR=d:\Users\sita\Documents\tasks\[batch]\ziparchive
+set SRCDIR=d:\Users\sita\Documents\tasks\batch\ziparchive
 
 if [%SRCDIR%\] EQU [%~dp0] goto :EOF
 
-copy /y "%SRCDIR%\*.tempbat" "%~dp0\*.bat"
+copy /y "%SRCDIR%\*.bak" "%~dp0\*.bat"
 
 if not exist "%~dp0\config.cmd" (
 	copy /y "%SRCDIR%\config.cmd" "%~dp0\*"
