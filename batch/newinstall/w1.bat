e650709b0f07a92fdf58273b48d0affa67082dcb
Sita Liu <chsliu@gmail.com>
2016-10-27 16:30:07 +0800

    Automated commit at 2016/10/27 週四  下午 04:30   on W1

--- batch/newinstall/w1.bat
+++ batch/newinstall/w1.bat
@@ -23,6 +23,8 @@ choco feature enable -nautoUninstaller
 
 cinst -y 7zip classic-shell cmder cpu-z crystaldiskinfo crystaldiskmark dropbox everything f.lux filebot firefox foxitreader git github google-chrome-x64 gow hg kodi line listary mp3tag netscan64 nimbletext notepadplusplus paint.net potplayer putty python2 skype teamviewer teraterm visualstudioexpress2008 windowsliveinstaller winmerge winscp wireshark
 
+cinst -y openssh -params '"/SSHServerFeature"'
+
 REM =================================
 
 mkdir %DOC%
