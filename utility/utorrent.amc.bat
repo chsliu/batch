0b9b3cb23f3eb53b34361a35aa02d0c58682cb5f
Sita Liu <chsliu@gmail.com>
2016-11-05 06:20:07 +0800

    Automated commit at 週六 2016/11/05  上午 06:20   on W1

--- utility/utorrent.amc.bat
+++ utility/utorrent.amc.bat
@@ -1,1 +1,14 @@
-@%~dp0\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%~dp1 " "5" "%1"
+@echo off
+
+set ROOT=%~dp0
+
+:main
+if [%1]==[] goto :EOF
+
+start call %ROOT%\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%~dp1 " "5" "%1" ^&^& exit
+
+sleep 1
+
+shift
+
+goto :main
