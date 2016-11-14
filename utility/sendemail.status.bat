b825b3a593fd7db310848d5692d73f3d56828020
Sita Liu <chsliu@gmail.com>
2016-11-15 07:39:07 +0800

    Automated commit at 週二 2016/11/15  上午 07:39   on W1

--- utility/sendemail.status.bat
+++ utility/sendemail.status.bat
@@ -369,6 +369,8 @@ for /l %%G in (0,1,11) do (
 	findstr "Temperature_Celsius" %temp%\%~n0-pd%%G-smart.txt					>>%LOG1%
 	findstr /C:"occurred at disk power-on lifetime" %temp%\%~n0-pd%%G-smart.txt			>>%LOG1%
 	findstr "FAILING_NOW" %temp%\%~n0-pd%%G-smart.txt						>>%LOG1%
+	findstr /C:"output error" %temp%\%~n0-pd%%G-smart.txt						>>%LOG1%
+	findstr /C:"Unknown USB" %temp%\%~n0-pd%%G-smart.txt						>>%LOG1%
   )
   
   del %temp%\%~n0-pd%%G-smart.txt
