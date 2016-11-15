ad12a218b7f9ddf17f678d502e34cfb98632644a
Sita Liu <chsliu@gmail.com>
2016-11-15 15:47:43 +0800

    Automated commit at 週二 2016/11/15  下午 03:47   on NB19

---
+++ taskschd/daily-HV2.bat
@@ -0,0 +1,4 @@
+REM =================================
+
+dir /s/b        E:\Shares > E:\Shares\Admin\hv2\listfile.e.txt
+dir /s/b/a:d    E:\Shares > E:\Shares\Admin\hv2\listdir.e.txt
--- taskschd/daily-HV4.bat
+++ taskschd/daily-HV4.bat
@@ -6,3 +6,7 @@ REM =================================
 
 call E:\Shares\Software\UNIX\Lubuntu\xbmc\userdata\sendemail.gitsync.bat
 
+REM =================================
+
+dir /s/b        E:\Shares > E:\Shares\Admin\hv4\listfile.e.txt
+dir /s/b/a:d    E:\Shares > E:\Shares\Admin\hv4\listdir.e.txt
