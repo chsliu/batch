686d5dbba504b3d1ba5ba0ce7787f1222f43aa1f
Sita Liu <chsliu@gmail.com>
2016-11-16 09:27:08 +0800

    Automated commit at 週三 2016/11/16  上午 09:27   on NB19

--- taskschd/daily-HV3.bat
+++ taskschd/daily-HV3.bat
@@ -1,8 +1,8 @@
 REM =================================
 
-start cmd /u /c "dir /s /b  	E:\Shares > E:\Shares\Admin\hv3\listfile.e.txt"
-start cmd /u /c "dir /s /b /a:d E:\Shares > E:\Shares\Admin\hv3\listdir.e.txt"
+start cmd /u /c "dir /s /b /a:d E:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listdir.e.txt"
+start cmd /u /c "dir /s /b  	E:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listfile.e.txt"
 
-start cmd /u /c "dir /s /b  	F:\Shares > E:\Shares\Admin\hv3\listfile.f.txt"
-start cmd /u /c "dir /s /b /a:d F:\Shares > E:\Shares\Admin\hv3\listdir.f.txt"
+start cmd /u /c "dir /s /b /a:d F:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listdir.f.txt"
+start cmd /u /c "dir /s /b  	F:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listfile.f.txt"
 
--- taskschd/daily-HV4.bat
+++ taskschd/daily-HV4.bat
@@ -8,6 +8,6 @@ call E:\Shares\Software\UNIX\Lubuntu\xbmc\userdata\sendemail.gitsync.bat
 
 REM =================================
 
-start cmd /u /c "dir /s /b  	E:\Shares > E:\Shares\Admin\hv4\listfile.e.txt"
-start cmd /u /c "dir /s /b /a:d E:\Shares > E:\Shares\Admin\hv4\listdir.e.txt"
+start cmd /u /c "dir /s /b /a:d E:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listdir.e.txt"
+start cmd /u /c "dir /s /b  	E:\Shares > E:\Shares\Admin\%COMPUTERNAME%\listfile.e.txt"
 
