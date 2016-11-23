43a5c3dc1cded83f036c405e58806d3a7b90656c
Sita Liu <chsliu@gmail.com>
2016-11-24 06:47:01 +0800

    Automated commit at Thu 11/24/2016  06:35 AM   on HV4

---
+++ rsync/daily-HV4.1.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call %~dp0\backup E:\Shares\Admin\hv4
+call %~dp0\backup E:\Shares\Photos
+call %~dp0\backup E:\Shares\Software
+call %~dp0\backup E:\Shares\Users
+
+rem goto exit
+
+:exit
