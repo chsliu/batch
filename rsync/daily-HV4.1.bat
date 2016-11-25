7a82321fcf24fddef5c913f5ebf0a11def8d6c47
Sita Liu <chsliu@gmail.com>
2016-11-26 06:42:58 +0800

    Automated commit at Sat 11/26/2016  06:35 AM   on HV4

--- rsync/daily-HV4.1.bat
+++ rsync/daily-HV4.1.bat
@@ -1,9 +1,9 @@
 @echo off
 
-call %~dp0\backup E:\Shares\Admin\hv4
-call %~dp0\backup E:\Shares\Photos
-call %~dp0\backup E:\Shares\Software
-call %~dp0\backup E:\Shares\Users
+call %~dp0\backup E:\Shares\Admin\hv4 pat-empty.txt
+call %~dp0\backup E:\Shares\Photos pat-empty.txt
+call %~dp0\backup E:\Shares\Software pat-empty.txt
+call %~dp0\backup E:\Shares\Users pat-empty.txt
 
 rem goto exit
 
