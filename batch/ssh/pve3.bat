d703016509163e9b6eb223ed5b757a7b627b2dea
Sita Liu <chsliu@gmail.com>
2017-02-17 17:29:08 +0800

    Automated commit at 週五 2017/02/17  下午 05:29   on W1

--- batch/ssh/pve3.bat
+++ batch/ssh/pve3.bat
@@ -1,2 +1,1 @@
-@ssh -i "D:\Users\sita\Dropbox\SSHKey\nb19_rsa" sita@192.168.1.233
-REM @ssh -i "D:\Users\sita\Dropbox\SSHKey\nb19_rsa" sita@%~n0
+@ssh -i "D:\Users\sita\Dropbox\SSHKey\nb19_rsa" sita@%~n0
