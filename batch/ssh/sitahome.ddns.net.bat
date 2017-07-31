338e9d28039744c4a6ac0cd326c9ea1dc3b3705c
Sita Liu <chsliu@gmail.com>
2017-07-31 11:48:31 +0800

    Automated commit at 2017/07/31 週一  上午 11:48   on NB22

--- batch/ssh/sitahome.ddns.net.bat
+++ batch/ssh/sitahome.ddns.net.bat
@@ -1,4 +1,10 @@
 @echo off
 
-REM ssh -i "D:\Users\sita\Dropbox\SSHKey\nb19_rsa" vagrant@%~n0
-ssh -i "D:\Users\sita\Dropbox\SSHKey\nb19_rsa" sita@%~n0
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
