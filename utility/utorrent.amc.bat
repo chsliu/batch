74089ac007968e254f1ee4eb903b3d9fc5f5ef86
Sita Liu <chsliu@gmail.com>
2017-10-17 06:40:47 +0800

    Automated commit at 2017/10/17 週二  上午 06:40   on VMW7

--- utility/utorrent.amc.bat
+++ utility/utorrent.amc.bat
@@ -1,12 +1,14 @@
 @echo off
 
 set ROOT=%~dp0
+set datapath=%~dp1
+IF %datapath:~-1%==\ SET datapath=%datapath:~0,-1%
 
 :main
 if [%1]==[] goto :EOF
 
-REM call %ROOT%\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%~dp1 " "5" "%1"
-start call %ROOT%\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%~dp1 " "5" "%1" ^&^& exit
+REM call %ROOT%\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%datapath" "5" "%1"
+start call %ROOT%\sendemail.filebot.amc.bat "" "11" "%~nx1" "single" "%~nx1" "%datapath%" "5" "%1" ^&^& exit
 
 sleep 1
 
