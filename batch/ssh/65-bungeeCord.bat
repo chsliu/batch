bc99676b25bb533d481db5098fd6f27a26363425
Sita Liu <chsliu@gmail.com>
2018-02-15 11:58:20 +0800

    Automated commit at 2018/02/15 週四  上午 11:58   on NB19

--- batch/ssh/65-bungeeCord.bat
+++ batch/ssh/65-bungeeCord.bat
@@ -9,7 +9,8 @@ del %TEMP%\temp.txt >nul
 set ROOT=D:\Users\sita\Nextcloud
 REM set HOST=40.86.176.36
 REM set HOST=104.42.17.80
-set HOST=ca65.westus.cloudapp.azure.com
+REM set HOST=ca65.westus.cloudapp.azure.com
+set HOST=ca65.southeastasia.cloudapp.azure.com
 
 echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
 "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
