b9fb7d0b2f2fc6237fb6251140db6ebc92e1f72b
Sita Liu <chsliu@gmail.com>
2018-03-07 16:30:06 +0800

    Automated commit at 2018/03/07 週三  下午 04:30   on W1

---
+++ batch/ssh/65-bungeeCord-TW1.bat
@@ -0,0 +1,14 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+REM set HOST=ca65.westus.cloudapp.azure.com
+set HOST=35.229.146.129
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
