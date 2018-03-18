6dfe23d1b0339f689daaaf59e118e21bc6504022
Sita Liu <chsliu@gmail.com>
2018-03-18 16:30:05 +0800

    Automated commit at 2018/03/18 週日  下午 04:30   on W1

---
+++ batch/gcloud/gcs-download.root.bat
@@ -0,0 +1,54 @@
+@echo off
+
+REM =================================
+goto :main
+
+REM =================================
+:DeQuote
+for /f "delims=" %%A in ('echo %%%1%%') do set %1=%%~A
+
+exit /b
+
+REM =================================
+:BASENAME
+set %2=%~nx1
+
+exit /b
+
+REM =================================
+:UHCPATH
+set _=%1
+call :DeQuote _
+call :BASENAME "%_%" __
+
+REM setlocal enabledelayedexpansion
+set %2=!_:%__%=!
+
+exit /b
+
+REM =================================
+:PATHNAME
+set %2=%~p1
+
+exit /b
+
+REM =================================
+:main
+
+REM =================================
+setlocal enabledelayedexpansion
+
+REM =================================
+set path=C:\Users\sita\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
+set bucket=gs://creeper-tw-backup
+call :UHCPATH %0 DST
+call :PATHNAME %0 BASE
+call :BASENAME %BASE:~0,-1% BASE
+
+REM =================================
+echo gsutil rsync %bucket%/ %DST%
+REM pause
+gsutil rsync %bucket%/ %DST%
+
+REM =================================
+pause
---
+++ batch/gcloud/gcs-upload.bat
@@ -0,0 +1,35 @@
+@echo off
+
+REM =================================
+goto :main
+
+REM =================================
+:BASENAME
+set %2=%~nx1
+
+exit /b
+
+REM =================================
+:PATHNAME
+set %2=%~p1
+
+exit /b
+
+REM =================================
+:main
+REM =================================
+if [%1]==[""] goto :EOF
+
+REM =================================
+set path=C:\Users\sita\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
+set bucket=gs://creeper-tw-backup
+call :PATHNAME "%0" BASE
+call :BASENAME "%BASE:~0,-1%" BASE
+call :BASENAME "%1" FILE
+
+REM =================================
+echo gsutil cp %1 %bucket%/%BASE%/%FILE%
+gsutil cp %1 %bucket%/%BASE%/%FILE%
+
+REM =================================
+pause
