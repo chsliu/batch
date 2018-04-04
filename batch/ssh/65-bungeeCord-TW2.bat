cee6059167140f5aaa394a5784f6d611656d9780
Sita Liu <chsliu@gmail.com>
2018-04-04 12:48:51 +0800

    Automated commit at 週三 2018/04/04  下午 12:48   on W1

---
+++ batch/ssh/65-bungeeCord-TW2.bat
@@ -0,0 +1,14 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+set HOST=tw2.creeper.tw
+
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%HOST%
