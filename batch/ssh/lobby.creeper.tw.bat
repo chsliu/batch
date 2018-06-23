3b48f6b88a2621db74c38e3e141c23e3265eadad
Sita Liu <chsliu@gmail.com>
2018-06-23 16:30:03 +0800

    Automated commit at 2018/06/23 週六  下午 04:30   on W1

---
+++ batch/ssh/lobby.creeper.tw.bat
@@ -0,0 +1,14 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+REM set HOST=35.233.174.157
+
+REM echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%~n0
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%~n0
+REM "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%HOST%
