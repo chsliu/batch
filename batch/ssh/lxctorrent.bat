bbdad57c411fa35caee9046b794cdd718388b574
Sita Liu <chsliu@gmail.com>
2018-11-27 21:12:04 +0800

    Automated commit at 2018/11/27 週二  下午 09:12   on NB22

---
+++ batch/ssh/lxctorrent.bat
@@ -0,0 +1,12 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
