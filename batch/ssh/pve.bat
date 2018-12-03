2f4d9c2c75baf9bb65e7ea7acf984f550c26a963
Sita Liu <chsliu@gmail.com>
2018-12-04 00:57:50 +0800

    Automated commit at 2018/12/04 週二  上午 12:57   on W1

--- batch/ssh/pve.bat
+++ batch/ssh/pve.bat
@@ -1,1 +1,12 @@
-@ssh -i "D:\Users\sita\Nextcloud\SSHKey\nb19_rsa" vagrant@%~n0
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" vagrant@%~n0
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" vagrant@%~n0
