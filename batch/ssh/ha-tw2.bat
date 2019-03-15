bd3c81ecf38867ac01cf73246048de496b52577a
Sita Liu <chsliu@gmail.com>
2019-03-15 15:08:13 +0800

    Automated commit at 2019/03/15 週五  下午 03:08   on NB22

---
+++ batch/ssh/ha-tw2.bat
@@ -0,0 +1,16 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+set ROOT=D:\Users\sita\Nextcloud
+set HOST=35.247.43.1
+
+REM echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%~n0
+REM "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%~n0
+
+echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%HOST%
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null sita@%HOST%
