55328d290384c908886e3c0522aa7bb2ec6538e1
Sita Liu <chsliu@gmail.com>
2021-04-22 16:30:04 +0800

    Automated commit at 2021/04/22 週四  下午 04:30   on NB22

--- batch/ssh/goodview.com.tw.bat
+++ batch/ssh/goodview.com.tw.bat
@@ -1,4 +1,5 @@
 @echo off
 
 REM ssh -i "D:\Users\sita\Nextcloud\SSHKey\nb19_rsa" vagrant@%~n0
-ssh -i "D:\Users\sita\Nextcloud\SSHKey\nb19_rsa" sita@%~n0
+REM ssh -i "D:\Users\sita\Nextcloud\SSHKey\nb19_rsa" sita@%~n0
+"C:\Program Files\Git\usr\bin\ssh.exe" -i "D:\Users\sita\Nextcloud\SSHKey\nb19_rsa" sita@%~n0
