568bcd31ead75018211cf8c42ddef695353c43e4
Sita Liu <chsliu@gmail.com>
2022-02-09 16:30:02 +0800

    Automated commit at 2022/02/09 週三  下午 04:30   on NB25

--- batch/ssh/pve6.bat
+++ batch/ssh/pve6.bat
@@ -6,7 +6,8 @@ call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
 set /p ROOT=<%TEMP%\temp.txt
 del %TEMP%\temp.txt >nul
 
-set ROOT=D:\Users\sita\Nextcloud
+REM set ROOT=D:\Users\sita\Nextcloud
+set ROOT=C:\Users\sita\Vault
 
 echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
 "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
