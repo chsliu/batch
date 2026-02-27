5aadeb2da017e73936bb22cbe25bceff901724b2
Sita Liu <chsliu@gmail.com>
2026-02-27 16:30:01 +0800

    Automated commit at 2026/02/27 週五  下午 04:30   on NB25

---
+++ batch/ssh/bazzite.bat
@@ -0,0 +1,17 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+REM set ROOT=D:\Users\sita\Nextcloud
+set ROOT=C:\Users\sita\Vault
+
+REM echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+REM "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+
+echo ssh bazzite@%~n0
+ssh bazzite@%~n0
+REM C:\Users\sita\AppData\Roaming\MobaXterm\slash\bin\sshpass.exe -p bazzite ssh bazzite@%~n0
---
+++ batch/ssh/jetson.bat
@@ -0,0 +1,16 @@
+@echo off
+
+chcp 950
+
+call %~dp0\getdropboxroot.cmd %LOCALAPPDATA%\Dropbox\info.json >%TEMP%\temp.txt
+set /p ROOT=<%TEMP%\temp.txt
+del %TEMP%\temp.txt >nul
+
+REM set ROOT=D:\Users\sita\Nextcloud
+set ROOT=C:\Users\sita\Vault
+
+REM echo "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+REM "C:\Program Files\Git\usr\bin\ssh.exe" -i "%ROOT%\SSHKey\nb19_rsa" sita@%~n0
+
+echo ssh jetson@%~n0
+ssh jetson@%~n0
